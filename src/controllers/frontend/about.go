44ab2a2bc883bc4a5006500bf4e925193c188fce
starboychina <admin@kansea.com>
2015-03-29 01:51:18 +0900

    临时页面

--- src/controllers/frontend/about.go
+++ src/controllers/frontend/about.go
@@ -1,35 +1,49 @@
 package frontend
 
 import (
-	"fmt"
+	//	"fmt"
 	"github.com/go-martini/martini"
 	"github.com/martini-contrib/render"
 	. "github.com/starboychina/martini-mvc/src/helpers/utilities"
-	"gopkg.in/mgo.v2/bson"
+	//	"github.com/starboychina/martini-mvc/src/models"
+	//	"gopkg.in/mgo.v2/bson"
 )
 
 func (c Contrller) AboutIndexGet(args martini.Params, r render.Render, db DbSession) {
-	//r.JSON(200, r)
-	for key, value := range args {
-		print(key)
-		print(value)
-	}
-	//render.Options
-	opt := render.HTMLOptions{
-		Layout: c.PathOptions.Layout,
-	}
-	f := args["id"] + "topcontroller"
-	//////////////////////////////////////////
-	result := Person{}
-	err := db.Read.C("people").Find(bson.M{"name": "Ale"}).One(&result)
-	if err == nil {
-		fmt.Println("Phone:", result.Phone)
-	} else {
-		fmt.Println("error:", err)
-	}
-	//////////////////////////////////////////
+	/*
+		//r.JSON(200, r)
+		for key, value := range args {
+			print(key)
+			print(value)
+		}
+		//render.Options
+		opt := render.HTMLOptions{
+			Layout: c.PathOptions.Layout,
+		}
+		f := args["id"] + "topcontroller"
+		//////////////////////////////////////////
+		user := models.User{}
+		user.Id = bson.NewObjectId()
+		user.Name = "real name"
+		addr1 := models.Shipping{}
+		addr1.Id = bson.NewObjectId()
+		addr1.Name = "ship_name"
+		user.Shippings = []models.Shipping{addr1}
 
-	r.HTML(200, c.ViewPath+"default/about", f, opt)
+		errinsert := db.Write(user).Insert(user)
+		fmt.Println("errinsert:", errinsert)
+
+		result := Person{}
+		err := db.Read(result).Find(bson.M{"name": "Ale"}).One(&result)
+		if err == nil {
+			fmt.Println("Phone:", result.Phone)
+		} else {
+			fmt.Println("error:", err)
+		}
+		//////////////////////////////////////////
+
+	*/
+	c.HTML(r, 200, "default/about", nil)
 }
 
 type Person struct {
